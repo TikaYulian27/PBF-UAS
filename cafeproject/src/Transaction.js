52c5f786a5b82f5839d51f1bd385b493c26950eb
TikaYulian27 <tikayulian27@gmail.com>
2020-05-11 16:13:55 +0700

    add transaction menu after click order

---
+++ cafeproject/src/Transaction.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Transaction extends Component {
+
+    render() {
+        return (
+          <div>
+            <h2>Detail Transaction</h2>
+            <p></p>
+            <p2>
+              Transaksi Anda akan segera diproses...<br />
+              Terimakasih<br/>
+              Selamat Menikmati<br/>
+              <br></br>
+              <br></br>
+              <br></br>
+              Délicioso Café<br></br>
+            Jl. Iswahyudi No.05 Tulungagung
+            </p2>
+          </div>
+        );
+      }
+    }
+ 
+export default Transaction;
